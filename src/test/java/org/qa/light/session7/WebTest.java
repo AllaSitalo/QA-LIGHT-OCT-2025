f5064615db15b27ccb1032c87e8a857e7888768a
aosipenko <aoskiev@gmail.com>
2025-10-25 11:05:00 +0200

    Session 7: Selenium

---
+++ src/test/java/org/qa/light/session7/WebTest.java
@@ -0,0 +1,31 @@
+package org.qa.light.session7;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+//TODO: write test that will to go allo.ua, search for iphone 17
+//TODO *: Assert that search has at least 3 results
+
+public class WebTest {
+
+    @Test
+    public void webTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com");
+        List<WebElement> cookieLink = driver.findElements(
+                By.xpath("//a[contains(@href, 'technologies/cookies')]"));
+        if (!cookieLink.isEmpty()) {
+            driver.findElements(By.tagName("button")).get(5).click();
+        }
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Java");
+        searchInput.sendKeys(Keys.ENTER);
+        driver.quit();
+    }
+}
