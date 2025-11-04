ed4e5229280f35b1b12cec0f3a5ce3f2d55434a5
AllaSitalo <allasitalo193@gmail.com>
2025-11-04 16:21:58 +0200

    Домашня робота - Апі з локацією, перевірка на Null для номера та назви

---
+++ src/test/java/org/qa/light/session9/HW_API.java
@@ -0,0 +1,33 @@
+package org.qa.light.session9;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+public class HW_API {
+    //add location to request
+    //Validate that street number and name are not null for 1st person generated
+    @Test
+    public void restTestsHW() {
+        ValidatableResponse r = RestAssured.given()
+                .queryParam("inc", "gender,name,nat,location")
+                .queryParam("results", "3")
+                .queryParam("noinfo")
+                .baseUri("https://randomuser.me/")
+                .basePath("/api")
+                .get()
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("results[0].location.street.number", Matchers.notNullValue())
+                .body("results[0].location.street.name", Matchers.notNullValue());
+
+        System.out.println("Локація першого користувача");
+        System.out.println(r.extract().response().prettyPrint());
+
+    }
+}
